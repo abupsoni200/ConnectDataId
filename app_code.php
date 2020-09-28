aff595e8e3d0b54ea9b168a1d534ad67bc74a248
anup soni <anupsoni200@gmail.com>
2020-09-28 12:53:38 +0530

    first commit

--- app_code.php
+++ app_code.php
@@ -2,43 +2,13 @@
 	"data":
 		[
 			{
-			"app_name":"TikBitz",
-			"package_name":"com.jsk.tikbitz",
+			"app_name":"Ok",
+			"package_name":"ok",
 			"app_icon":"http://rbinfotech.in/NewWayAdShow/AllIcons/tikbitz.png"
 			},
 			{
-			"app_name":"Car Launcher For Android",
-			"package_name":"mobi.car.launcher",
-			"app_icon":"http://rbinfotech.in/NewWayAdShow/AllIcons/carlauncherandroid.png"
-			},
-			{
-			"app_name":"Teen Patti Guide",
-			"package_name":"com.artds.teenpattiguide",
-			"app_icon":"http://rbinfotech.in/2018/ArtDSApps/AllIcons/teenpattiguide.png"
-			},
-			{
-			"app_name":"App Update Checker",
-			"package_name":"com.jvr.dev.softwareupdate",
-			"app_icon":"http://rbinfotech.in/2016/JVRDevelopers/allicons/softwareupdate.png"
-			},
-			{
-			"app_name":"Magnifying Glass Simulator",
-			"package_name":"com.jvr.dev.magnifying.glass.light.camera",
-			"app_icon":"http://rbinfotech.in/2016/JVRDevelopers/allicons/magnifying.png"
-			},
-			{
-			"app_name":"Live Traffic Map - Navigation",
-			"package_name":"com.jvr.live.traffic.details",
-			"app_icon":"http://rbinfotech.in/NewWayAdShow/AllIcons/livetrafficmapnavigation.png"
-			},
-			{
-			"app_name":"Voice GPS Navigation Map",
-			"package_name":"com.angel.voice.navigation",
-			"app_icon":"http://rbinfotech.in/2019/LittleAngel/AllAppIcon/voicenavigationmap.png"
-			},
-			{
-			"app_name":"GPS Camera-Photo With Location",
-			"package_name":"com.artds.gps.camera",
+			"app_name":"Ok",
+			"package_name":"ok",
 			"app_icon":"http://rbinfotech.in/2018/ArtDSApps/AllIcons/gpscamera.png"
 			}
 		]
