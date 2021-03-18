68f1569941666f56906f077bad103481419c2d7d
anup soni <anupsoni200@gmail.com>
2021-03-18 11:23:24 +0530

    first commit

--- SoftwareUpdaterId/app_ad_code_id.php
+++ SoftwareUpdaterId/app_ad_code_id.php
@@ -9,7 +9,7 @@
 			"adappopen_id":"ca-app-pub-3940256099942544/1033173712",
 			"adbanner_id":"ca-app-pub-3940256099942544/6300978111",
 			"adbanner_rect_id":"ca-app-pub-3940256099942544/6300978111",
-			"adinterstial_id":"",
+			"adinterstial_id":"ca-app-pub-3940256099942544/1033173712",
 			"adnativebanner_id":"ca-app-pub-3940256099942544/2247696110",
 			"adfbbanner_id":"IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID",
 			"adfbinterstial_id":"YOUR_PLACEMENT_ID",
