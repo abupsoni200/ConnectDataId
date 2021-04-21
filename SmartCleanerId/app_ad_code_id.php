ad313b87b2faea5605c9ac11c709f5bd09618ce9
anup soni <anupsoni200@gmail.com>
2021-04-21 18:35:48 +0530

    first commit

---
+++ SmartCleanerId/app_ad_code_id.php
@@ -0,0 +1,21 @@
+{
+	"ad_id":
+		[
+			{
+			"g_ad":"1",
+			"fb_ad":"0",
+			"adpub_id":"pub-7377460191923303",
+			"adapp_id":"ca-app-pub-7377460191923303~9680814941",
+			"adappopen_id":"ca-app-pub-7377460191923303/5003885170",
+			"adbanner_id":"ca-app-pub-7377460191923303/1802324926",
+			"adbanner_rect_id":"ca-app-pub-7377460191923303/2796879457",
+			"adinterstial_id":"ca-app-pub-7377460191923303/6671508226",
+			"adnativebanner_id":"ca-app-pub-7377460191923303/6834966238",
+			"adfbbanner_id":"277398297416028_277398690749322",
+			"adfbinterstial_id":"277398297416028_277400307415827",
+			"adfbnative_id":"277398297416028_277402857415572",
+			"adfbnative_banner_id":"277398297416028_277403227415535",		
+			"adfbrectangle_banner_id":"277398297416028_277401614082363"
+			}
+		]
+}
