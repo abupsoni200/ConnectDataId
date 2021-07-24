4ade7ca8e02d9c5c48fad843561ad6a5b913cfe8
Anup Soni <72005155+abupsoni200@users.noreply.github.com>
2021-07-24 17:08:30 +0530

    Add files via upload

---
+++ InvitationCardMakerId/app_ad_code_id.php
@@ -0,0 +1,21 @@
+{
+	"ad_id":
+		[
+			{
+			"g_ad":"1",
+			"fb_ad":"0",
+			"adpub_id":"pub-7377460191923303",
+			"adapp_id":"ca-app-pub-7377460191923303~4400036860",
+			"adappopen_id":"ca-app-pub-7377460191923303/4303833945",
+			"adbanner_id":"ca-app-pub-7377460191923303/8062922036",
+			"adbanner_rect_id":"ca-app-pub-7377460191923303/7001955957",
+			"adinterstial_id":"ca-app-pub-7377460191923303/6099937823",
+			"adnativebanner_id":"ca-app-pub-7377460191923303/2324344341",
+			"adfbbanner_id":"IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID",
+			"adfbinterstial_id":"YOUR_PLACEMENT_ID",
+			"adfbnative_id":"YOUR_PLACEMENT_ID",
+			"adfbnative_banner_id":"YOUR_PLACEMENT_ID",		
+			"adfbrectangle_banner_id":"YOUR_PLACEMENT_ID"
+			}
+		]
+}
