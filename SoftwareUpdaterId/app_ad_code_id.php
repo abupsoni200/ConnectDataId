9f521f41151f9af4b3a8aeab4368b620258f32f4
Anup Soni <72005155+abupsoni200@users.noreply.github.com>
2022-11-27 14:07:43 +0530

    Update app_ad_code_id.php

--- SoftwareUpdaterId/app_ad_code_id.php
+++ SoftwareUpdaterId/app_ad_code_id.php
@@ -2,7 +2,7 @@
 	"ad_id":
 		[
 			{
-			"g_ad":"0",
+			"g_ad":"1",
 			"fb_ad":"0",
 			"adpub_id":"pub-7377460191923303",
 			"adapp_id":"ca-app-pub-7377460191923303~6558514502",
@@ -16,7 +16,7 @@
 			"adfbnative_id":"743434966235438_743436029568665",
 			"adfbnative_banner_id":"743434966235438_743442156234719",		
 			"adfbrectangle_banner_id":"743434966235438_743443286234606",
-			"adShow_time_duration":"25"
+			"adShow_time_duration":"32"
 			}
 		]
 }
